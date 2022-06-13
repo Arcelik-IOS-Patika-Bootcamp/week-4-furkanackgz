56d2b97990b3857c46361b2aa9a8b625563468a0
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-13 12:31:26 +0300

    Create MainView and connect its UI components to its VC

---
+++ week4_Assignment/week4_Assignment/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  week4_Assignment
+//
+//  Created by Furkan Açıkgöz on 12.06.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var labelFavorites: UILabel!
+    @IBOutlet weak var buttonAdd: UIButton!
+    @IBOutlet weak var tableViewFavorites: UITableView!
+    @IBOutlet weak var buttonAscendants: UIButton!
+    @IBOutlet weak var buttonDescendants: UIButton!
+    @IBOutlet weak var tableViewAscDes: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonAddTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonAscendantsTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonDescendants(_ sender: Any) {
+        
+    }
+    
+    
+}
+
