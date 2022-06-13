d2dcbd32b6fff534e5109a96c78a87b95aeec83e
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-13 15:10:33 +0300

    Create CryptoCurrencies page and connect UI elements to its VC

---
+++ week4_Assignment/week4_Assignment/CryptoCurrencies/CryptoCurrenciesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CryptoCurrenciesViewController.swift
+//  week4_Assignment
+//
+//  Created by Furkan Açıkgöz on 13.06.2022.
+//
+
+import UIKit
+
+class CryptoCurrenciesViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
