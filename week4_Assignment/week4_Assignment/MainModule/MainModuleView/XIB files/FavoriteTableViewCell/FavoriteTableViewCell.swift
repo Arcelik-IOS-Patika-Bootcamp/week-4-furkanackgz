a52db567a91b04eb27ca94bb46d23517abe50c4c
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-15 19:52:42 +0300

    Configure appearance of containerView in FavoriteTableViewCell

--- week4_Assignment/week4_Assignment/MainModule/MainModuleView/XIB files/FavoriteTableViewCell/FavoriteTableViewCell.swift
+++ week4_Assignment/week4_Assignment/MainModule/MainModuleView/XIB files/FavoriteTableViewCell/FavoriteTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class FavoriteTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var labelSymbol: UILabel!
     @IBOutlet weak var labelVolume: UILabel!
     @IBOutlet weak var labelPrice: UILabel!
@@ -17,7 +18,12 @@ class FavoriteTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        // Configuring container view apperance.
+        containerView.backgroundColor = .white
+        containerView.layer.cornerRadius = 8
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor.lightGray.cgColor
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
